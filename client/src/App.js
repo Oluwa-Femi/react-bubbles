7963f38cb80bf4f8544ae09c622e47c3fdef62ee
unknown <oluwatolablessing@gmail.com>
2019-10-18 09:27:38 +0100

    axiosWithAuth created

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,14 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import Login from "./components/Login";
 import "./styles.scss";
+import PrivateRoute from "./Routes/PrivateRoute"
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Route exact path="/" component={Login} />
+        <PrivateRoute path="/colors" />
         {/* 
           Build a PrivateRoute component that will 
           display BubblePage when you're authenticated 
